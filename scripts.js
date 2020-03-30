9c7b4edd02fbae1f3319fa1b2ced55b3a3231412
taiwoadeniji <taiwoadeniji19@gmail.com>
2020-03-29 19:58:11 -0600

    Basic Calculator with comments

--- scripts.js
+++ scripts.js
@@ -1,3 +1,6 @@
+//Create a function that takes two numbers and a mathematical operator + - / * and //
+//will perform a calculation with the given numbers.//
+
 function calculator(num1, operator, num2) {
     if (num2 === 0) { return "Can't divide by 0!"; }
     else {
