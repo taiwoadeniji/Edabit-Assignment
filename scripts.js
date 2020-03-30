3ee9b3c5e6039982b471730e7086ce76154926ee
taiwoadeniji <taiwoadeniji19@gmail.com>
2020-03-29 19:50:58 -0600

    basic calculator

---
+++ scripts.js
@@ -0,0 +1,17 @@
+function calculator(num1, operator, num2) {
+    if (num2 === 0) { return "Can't divide by 0!"; }
+    else {
+        switch (operator) {
+            case "+":
+                return num1 + num2;
+            case "-":
+                return num1 - num2;
+            case "*":
+                return num1 * num2;
+            case "/":
+                return num1 / num2;
+        }
+    }
+}
+// This codes runs on edabit too.//
+
